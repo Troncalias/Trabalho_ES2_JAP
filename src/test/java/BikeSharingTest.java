d63cfe6926bbcff22a861fca5ec89f496fe02c0e
António Alves <troncalias@gmail.com>
2018-11-14 16:23:19 +0000

    'testesRegisterUser1

--- src/test/java/BikeSharingTest.java
+++ src/test/java/BikeSharingTest.java
@@ -1,12 +1,27 @@
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import Exceptions.UserAlreadyExists;
 import org.junit.jupiter.api.Test;
 
 public class BikeSharingTest {
 
     @Test
-    void test1(){
+    public void registerUser_AlreadyExist(){
+        assertThrows(UserAlreadyExists.class,
+                ()->{
+                    BikeRentalSystem i = new BikeRentalSystem(10);
+                    i.registerUser(0,"nome", 2);
+                    i.registerUser(0,"nome", 2);
+                });
+    }
 
+    @Test
+    public void registerUser_Add() throws UserAlreadyExists {
         BikeRentalSystem i = new BikeRentalSystem(10);
+        i.registerUser(0,"nome", 2);
+        i.registerUser(1,"nome", 2);
+        assertEquals(i.getUsers().size(),2);
     }
 
 }
