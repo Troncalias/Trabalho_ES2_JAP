9cbb5e38d8b6b1c8858423e2533dc89773fd7de0
DESKTOP-Tiago <8150233@estg.ipp.pt>
2018-11-14 17:09:43 +0000

    testes ao credit

--- src/test/java/BikeSharingTest.java
+++ src/test/java/BikeSharingTest.java
@@ -1,8 +1,11 @@
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import Exceptions.UserAlreadyExists;
+import Models.User;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BikeSharingTest {
 
@@ -21,7 +24,32 @@ public class BikeSharingTest {
         BikeRentalSystem i = new BikeRentalSystem(10);
         i.registerUser(0,"nome", 2);
         i.registerUser(1,"nome", 2);
-        assertEquals(i.getUsers().size(),2);
+        assertEquals(2, i.getUsers().size());
     }
 
+    @Test
+    public void credit_add() throws UserAlreadyExists {
+        BikeRentalSystem i = new BikeRentalSystem(10);
+        i.registerUser(1, "nome", 1);
+        i.addCredit(1, 20);
+        i.addCredit(1, 30);
+        List<User> list = i.getUsers();
+        assertEquals(50, list.get(0).getCredit());
+
+    }
+
+    @Test
+    public void verifycreditzero() throws UserAlreadyExists {
+        BikeRentalSystem i = new BikeRentalSystem(10);
+        i.registerUser(1, "nome", 1);
+        assertFalse(i.verifyCredit(1));
+    }
+
+    @Test
+    public void verifycreditposivite() throws UserAlreadyExists {
+        BikeRentalSystem i = new BikeRentalSystem(10);
+        i.registerUser(1, "nome", 1);
+        i.addCredit(1, 500);
+        assertTrue(i.verifyCredit(1));
+    }
 }
